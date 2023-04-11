8155dba3f52c12f1326e703f34a20d323ee55951
trixdev <129802454+trixdev@users.noreply.github.com>
2023-04-11 17:28:15 -0500

    Add files via upload

---
+++ DataTypeExercises.js
@@ -0,0 +1,111 @@
+// * DATA TYPE EXERCISES
+// We have 8 Data Types: Number, String, Boolean, Bigint, Undefined, Null, Symbol, and Object
+
+// * Strings
+// TODO Create a variable of type String using double quotes ""
+let doubleQuotes;
+doubleQuotes = "Hello World";
+
+// TODO Create a variable of type String using single quotes ''
+let singleQuotes;
+singleQuotes = 'Hello World';
+
+
+// TODO Create a variable of type String and set it to your name
+// TODO Create a variable of type String using backticks `` and insert yourFullName into it
+let yourFullName= "Carmen Dela Pena";
+let backticks = 'Hi my name is:' ;
+
+
+// TODO Add (and log) exampleString to exampleNumber1
+let exampleString = 'Hello world';
+let exampleNumber1 = 50;
+let exampleNumber2 = 50;
+
+// TODO Add (and log) exampleNumber1 to exampleString
+
+// TODO Add (and log) exampleNumber1 to exampleNumber2, then add it to exampleString
+
+// * Numbers
+
+// TODO Create a number with decimals
+let float =2.5;
+
+
+// TODO Create a number without decimals
+let integer = 2;
+
+// TODO Create a number using expontential notation (12300 === 123e2)
+let expontential ;
+
+// * Booleans
+
+// TODO Create a boolean using true
+let boolean1=true;
+
+// TODO Create a boolean using false
+let boolean2=false;
+
+// TODO Create a boolean by comparing the value of two numbers
+let x = 20;
+let y = 20;
+let xEqualsY;
+
+// * Bigints
+
+// TODO Give largeNumber a value of 100000000000000000000 and bigInteger a value of100000000000000000000n 
+let largeNumber=10000000000000;    // Number
+let bigInteger=300000000000000n;    // Bigint
+
+// TODO Try to add these variables together in the console.
+// ? What happens?
+
+// TODO Create a new variable called safe which is equal to Number.MAX_SAFE_INTEGER (9007199254740991)
+
+// TODO Console.log() the sum of safe and largeNumber.
+// ? What happens? Does that look correct?
+let safe = Number.MAX_SAFE_INTEGER;
+let large = largeNumber;
+
+// * Objects
+
+// TODO Create an object that represents yourself (name, age, hairColor)
+const trix = {name:"trix", age:36, hairColor:"black"};
+let meObject;
+
+// TODO Console.log() the value of the properties of your meObject
+
+// * Undefined
+let undef = undefined;
+
+
+// TODO Declare a variable, but do not assign a value to it. Console.log() that varaible
+
+// TODO Empty another variable you have created by assigning it Undefined
+
+
+// * Null
+
+// TODO Set person to null to clear it
+let person = { name:"Eggbert" };
+person = null;
+
+// * Symbol
+
+// TODO Create a Symbol of your name
+let myName1;
+
+
+
+// TODO Create a second Symbol of your name
+let myName2;
+
+
+// TODO Compare (===) myName1 to myName2 and console.log() it. Are they equal?
+
+// TODO Create a Symbol containing a String and use it as a property for personObject
+let personObject;
+personObject.name = "Eggbert";
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# trix
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=10819037&assignment_repo_type=AssignmentRepo)
+# Module4-DataTypes
+
+Over the next few days we're going to be taking a deeper look at Strings, Numbers, and getting our first exposure to Arrays.
+
+All PowerPoints will be available in the slides folder.
